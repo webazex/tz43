d7fd09691c816b0d33bf587d6b9371454863f460
webazex <webazex@gmail.com>
2026-08-05 13:54:28 +0300

    fix: params config fixes

--- config/params.php
+++ config/params.php
@@ -1,7 +1,26 @@
 <?php
 
-return [
+declare(strict_types=1);
+
+use yii\helpers\ArrayHelper;
+
+$params = [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+
+    'api' => [
+        'accessTokens' => [],
+    ],
 ];
+
+$localParamsFile = __DIR__ . '/local/params.php';
+
+if (is_file($localParamsFile)) {
+    $params = ArrayHelper::merge(
+        $params,
+        require $localParamsFile
+    );
+}
+
+return $params;
