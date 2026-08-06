e7ab4855e8c411ebec9c49a6e4895c069bc62c12
webazex <webazex@gmail.com>
2026-08-06 11:01:21 +0300

    fix: fix ЧПУ links

--- config/rules.php
+++ config/rules.php
@@ -3,7 +3,18 @@
 declare(strict_types=1);
 
 return [
+    // Frontend
+    '' => 'site/index',
+
+    'GET,POST login' => 'site/login',
+    'POST logout' => 'site/logout',
+
+    'GET,POST contact' => 'site/contact',
+    'GET about' => 'site/about',
     'GET dashboard' => 'site/dashboard',
 
+    'GET captcha' => 'site/captcha',
+
+    // API
     'POST clients' => 'api/client/create',
 ];
