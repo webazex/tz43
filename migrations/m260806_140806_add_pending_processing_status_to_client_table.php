683798a98d0b7c0d56c0ff402fc7128eb2ac8280
webazex <webazex@gmail.com>
2026-08-06 17:48:20 +0300

    featured: top-up functionality, part two: added migration

---
+++ migrations/m260806_140806_add_pending_processing_status_to_client_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\db\Migration;
+
+class m260806_140806_add_pending_processing_status_to_client_table extends Migration
+{
+    public function safeUp(): void
+    {
+        $this->addColumn(
+            '{{%client}}',
+            'pending_processing_status',
+            $this->string(16)
+                ->notNull()
+                ->defaultValue('idle')
+                ->after('status')
+        );
+    }
+
+    public function safeDown(): void
+    {
+        $this->dropColumn('{{%client}}', 'pending_processing_status');
+    }
+}
