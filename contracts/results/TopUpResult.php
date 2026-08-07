c63e98bca239d297803040af3e09a60439031d28
webazex <webazex@gmail.com>
2026-08-07 13:26:02 +0300

    featured: top-up functionality part five: add result topUp contract

---
+++ contracts/results/TopUpResult.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\contracts\results;
+
+final readonly class TopUpResult
+{
+    public function __construct(
+        public string $oldBalance,
+        public string $newBalance,
+        public bool $pendingProcessingStarted,
+    ) {
+    }
+}
